7e68b14ce0ae3bf372ac09129caf93e098ac320c
gksruf2848 <4455qqq@naver.com>
2022-03-02 23:49:02 +0900

    .

--- src/Hex_1550.java
+++ src/Hex_1550.java
@@ -1,10 +1,10 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Hex_1550 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String st = br.readLine();
-            
-
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+        System.out.println(Integer.parseInt(st,16));
     }
 }
