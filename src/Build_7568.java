8a3e1340c333f8620f4031e55ff1b50e4837cc48
gksruf2848 <4455qqq@naver.com>
2022-03-08 23:29:33 +0900

    .

--- src/Build_7568.java
+++ src/Build_7568.java
@@ -2,6 +2,25 @@ import java.io.*;
 
 public class Build_7568 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int weight[] = new int[N];
+        int height[] = new int[N];
 
+        //입력 받는 부분
+        for(int i = 0; i < N; i++) {
+            String tmp[] = br.readLine().split(" ");
+            weight[i] = Integer.parseInt(tmp[0]);
+            height[i] = Integer.parseInt(tmp[1]);
+        }
+
+        for(int i = 0; i < N; i++) {
+            int score = 1;
+            for(int j = 0; j < N; j++) {
+                if (weight[i] < weight[j] && height[i] < height[j])
+                    score++;
+            }
+            System.out.println(score);
+        }
     }
 }
