4c60ed0c944b89d3d6df4ed11a27b4df19040a49
gksruf2848 <4455qqq@naver.com>
2022-03-09 23:46:54 +0900

    .

---
+++ src/WriteNumbers_1748.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class WriteNumbers_1748 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
